f4ec3bc4e8e48e27fada2fb5431452c8f993c800
Yash-1995 <yashgold12@gmail.com>
2025-07-12 14:38:11 +0000

    Refactored the string_sanitizer method and Fixed failing test case to support multiple special delimeter

--- app/string_calculator_factory.rb
+++ app/string_calculator_factory.rb
@@ -13,19 +13,9 @@ class StringCalculatorFactory
 
     def string_sanitizer(input)
         return [] if input.empty?
-        sanatized_numbers = if input.start_with?("//")
-            delimiter_section, number_section = input.split("\n", 2)
 
-            if delimiter_section.match?(/\[.+\]/)
-                delimiter = delimiter_section.match(/\[(.+?)\]/)[1]
-                numbers = number_section.split(/#{Regexp.escape(delimiter)}/).map(&:to_i)
-            else
-                delimiter = delimiter_section[2]
-                numbers = number_section.split(/#{Regexp.escape(delimiter)}/).map(&:to_i)
-            end
-        else
-            input.split(/,|\n/).map(&:to_i)
-        end
+        sanatized_numbers = input.scan(/-?\d+/).map(&:to_i)
+        
         raise_error_if_found_negative_numbers(get_negative_numbers(sanatized_numbers))
 
         sanatized_numbers.select{ _1 <= 1000 }
