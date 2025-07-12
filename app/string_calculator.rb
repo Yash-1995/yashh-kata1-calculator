4093a02959e8264d07c63b48c146f4e5f375ff04
Yash-1995 <yashgold12@gmail.com>
2025-07-12 14:56:13 +0000

    Made the main method dynamic and to accept input from terminal

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -2,6 +2,11 @@ require_relative "add_string_calculator"
 
 class StringCalculator
   def add(numbers)
-    AddStringCalculator.new("").call
+    AddStringCalculator.new(numbers).call
   end
 end
+
+
+puts "Welcome to KATA 1 String Calculator, kindly give input below: \n"
+input = gets.chomp
+puts StringCalculator.new.add(input)
