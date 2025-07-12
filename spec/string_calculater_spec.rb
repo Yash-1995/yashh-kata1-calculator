347893d9bd0b45b829a00ace1bc45b0476035d79
Yash-1995 <yashgold12@gmail.com>
2025-07-12 10:43:30 +0000

    Added failing test case for passing one number and return the same

--- spec/string_calculater_spec.rb
+++ spec/string_calculater_spec.rb
@@ -2,8 +2,13 @@ require_relative '../app/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.new.add("")).to eq(0)
+    it "returns 0 for an empty string" do
+      expect(AddStringCalculator.new("").call).to eq(0)
     end
+
+
+it "returns the number itself if only one number is provided" do
+  expect(AddStringCalculator.new("5").call).to eq(5)
+end
   end
 end
