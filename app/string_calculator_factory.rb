8db85531a845d98a006c8f1c0ce268a0fa2c5fc5
Yash-1995 <yashgold12@gmail.com>
2025-07-12 12:35:03 +0000

    Fixed failing test for raising exception on passing any negative number

--- app/string_calculator_factory.rb
+++ app/string_calculator_factory.rb
@@ -1,27 +1,40 @@
 require 'byebug'
 
 class StringCalculatorFactory
-  def initialize(input)
-    @input = input
-  end
-
-  def call
-    numbers = string_sanitizer(@input)
-    calculator = create_calculator(numbers)
-    calculator.calculate
-  end
-
-  def string_sanitizer(input)
-    return [] if input.empty?
-    if input.start_with?("//")
-        delimiter, numbers = input.match(%r{//(.)\n(.*)}).captures
-        numbers.split(/#{Regexp.escape(delimiter)}/).map(&:to_i)
-    else
-        input.split(/,|\n/).map(&:to_i)
+    def initialize(input)
+        @input = input
     end
-  end
 
-  def create_calculator(_numbers)
-    raise NotImplementedError
-  end
+    def call
+        numbers = string_sanitizer(@input)
+        calculator = create_calculator(numbers)
+        calculator.calculate
+    end
+
+    def string_sanitizer(input)
+        return [] if input.empty?
+        sanatized_numbers = if input.start_with?("//")
+            delimiter, numbers = input.match(%r{//(.)\n(.*)}).captures
+            numbers.split(/#{Regexp.escape(delimiter)}/).map(&:to_i)
+        else
+            input.split(/,|\n/).map(&:to_i)
+        end
+        raise_error_if_found_negative_numbers(get_negative_numbers(sanatized_numbers))
+        sanatized_numbers
+    end
+
+    # We can move this logic to add string calculator as well if we are going to add another calculator but with different cases
+    def get_negative_numbers(sanatized_numbers)
+        sanatized_numbers.select{ _1.negative? }
+    end
+
+    def raise_error_if_found_negative_numbers(negative_numbers)
+        if negative_numbers.any?
+            raise "Negative Numbers are not allowed. You have passed negative numbers: #{negative_numbers.join(",")}"
+        end
+    end
+
+    def create_calculator(_numbers)
+        raise NotImplementedError
+    end
 end
